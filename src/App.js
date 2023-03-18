5705e7f3db10cae53e4c1412d9e844e9b0551009
vaibhavsingh-workstation <vaibhavsingh31390@gmail.com>
2023-03-19 04:05:39 +0530

    [ADD] Routes, Pages, Register Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
-}
-
-export default App;
+};
---
+++ src/pages/Register.jsx
@@ -0,0 +1,181 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import "./pages.scss";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "./../assets/logo.svg";
+import {ToastContainer, toast} from "react-toastify"; 
+import "react-toastify/dist/ReactToastify.css"; 
+import axios from "axios";
+function Register() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if(handleValidation()){
+      const { username, email, password, confirmPassword } = values;
+      const {data} = await axios.post()
+    }
+    alert("form");
+  };
+
+  const handleValidation = () => {
+    // const ValidationLowerc = /^(?=.*[a-z])$/;
+    // const ValidationUpperc = /^(?=.*[A-Z])$/;
+    // const ValidationNum = /^(?=.*\d)$/;
+    // const ValidationSpecialc = /^(?=.*[!@#$%^&*()_+])$/;
+    // const ValidationMinc = /^{10,}$/;
+    // switch (password) {
+    //   case ValidationLowerc.test(password):
+    //     alert("Lower case");
+    //     break;
+    //   case ValidationUpperc.test(password):
+    //     alert("Lower case");
+    //     break;
+    //   case ValidationNum.test(password):
+    //     alert("Lower case");
+    //     break;
+    //   case ValidationSpecialc.test(password):
+    //     alert("Lower case");
+    //     break;
+    //   case ValidationMinc.test(password):
+    //     alert("Lower case");
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+
+
+    const { username, email, password, confirmPassword } = values;
+    if(password !== confirmPassword){
+      toast.error("Password & confirm password don't match.", {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: false,
+        theme:"colored",
+      })
+      return false
+    } else if(username.length < 3){
+      toast.error("Username should be greater then 3 characters.", {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: false,
+        theme:"colored",
+      })
+      return false
+    } else if(password.length < 8){
+      toast.error("Password should be greater then or equal to 8 characters.", {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: false,
+        theme:"colored",
+      })
+      return false
+    } else if(email === ""){
+      toast.error("Email is required", {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: false,
+        theme:"colored",
+      })
+      return false
+    }
+    return true;
+  };
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  return (
+    <>
+      <FromContainer>
+        <form id="register_Form" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={logo} alt="" srcset="" />
+            <h1>Chatify</h1>
+          </div>
+          <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username_Reg"
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email_Reg"
+              onChange={(e) => handleChange(e)}
+              
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password_Reg"
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirm_Password_Reg"
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+
+          <span id="form-control-buttom">
+            <button type="submit">Create Account</button>
+            <span>
+              Already Have An Account ?<Link to="/login"> Login</Link>
+            </span>
+          </span>
+
+          <div className="error-message">
+            <ul>
+              <li>Some error</li>
+            </ul>
+          </div>
+        </form>
+      </FromContainer>
+      <ToastContainer toastStyle={{ backgroundColor: "#ffffff", color:"#3E54AC"}} />
+    </>
+  );
+}
+
+const FromContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #3e54ac;
+`;
+
+export default Register;
